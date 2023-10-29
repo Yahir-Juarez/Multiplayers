2dfca2e527807f27cec0169396115f0ecf42f75a
Yahir-Juarez <idv22c.yjuarez@uartesdigitales.edu.mx>
2023-10-29 15:22:04 -0600

    se agrego la funcion countSetBits()
    Esta funcion acepta valores de tipo string

--- Multiplayers/Packages.cpp
+++ Multiplayers/Packages.cpp
@@ -8,27 +8,40 @@ int countSetBits(T data)
 	{
 		setBits += (data & (1 << i)) ? 1 : 0;
 	}
-	cout << setBits << endl;
 
 	return setBits;
 }
 
 int countSetBits(const void* pData, int sizeofData)
 {
+	int setBits = 0;
 
+	const char* pDataInBytes = reinterpret_cast<const char*>(pData);
+
+	for (int i = 0; i < sizeofData; ++i)
+	{
+		for (int j = 0; j < 8; ++j)
+		{
+			setBits += (pDataInBytes[i] & (1 << j)) ? 1 : 0;
+		}
+	}
+
+	return setBits;
 }
 
 Checksum getChecksum(const void* pData, int sizeofData)
 {
-
+	Checksum x = 0;
+	return x;
 }
 
 Package getPackage(const void* pData, int sizeofData)
 {
-
+	Package x;
+	return x;
 }
 
 bool isPackageValid(const Package& pack)
 {
-
+	return 0;
 }
